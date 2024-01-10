05ff3948caf8778f618f747f967d76b02c2de2c5
navesar <150476783+navesar@users.noreply.github.com>
2024-01-10 12:20:42 +0200

    Update AntiVirus.py
    broke down the handle func to handle -> scan -> handle_threats -> deal

--- av/AntiVirus.py
+++ av/AntiVirus.py
@@ -1,6 +1,6 @@
 import platform, subprocess, time
 from utils import Scanner, Drive
-from tkinter import Tk, messagebox
+from tkinter import Tk, messagebox, Button, Label
 
 
 class AntiVirus:
@@ -51,23 +51,62 @@ class AntiVirus:
             message=f"A new flash drive named '{flash_drive.name}' detected\nDo you want to scan it?"
         )
         if should_scan:
-            pot_threats = self.scanner.scan(flash_drive)
-            for dir_name in pot_threats:
-                further_action = messagebox.askyesno(
-                    title="Potential Threat Detected",
-                    message=f"The flash drive '{flash_drive.name}' has a file called {dir_name} that may be harmful\n"
-                            f"Do you want to take further action?"
-                )
-                if further_action:
-                    self.deal(flash_drive, dir_name)
+            self.scan(flash_drive)
+
+    def scan(self, flash_drive):
+        pot_threats = self.scanner.scan(flash_drive)
+        if len(pot_threats) == 0:
+            messagebox.showinfo(
+                title="Scanning Complete",
+                message=f"The scan of '{flash_drive.name}' is complete\nThere is no potential threats"
+            )
+        else:
+            messagebox.showinfo(
+                title="Scanning Complete",
+                message=f"The scan of '{flash_drive.name}' is complete\n"
+                        f"Found {len(pot_threats)} potential threats"
+            )
+            self.handle_threats(flash_drive, pot_threats)
+
+    def handle_threats(self, flash_drive, pot_threats):
+        for dir_name, dir_path in pot_threats:
+            further_action = messagebox.askyesno(
+                title="Potential Threat Detected",
+                message=f"The flash drive '{flash_drive.name}' has a file called {dir_name} that may be harmful\n"
+                        f"Do you want to take further action?"
+            )
+            if further_action:
+                self.deal(dir_path)
 
     def update_connected_devices(self, curr_connected_flash_drives):
         self.connected_drives = [flash_drive for flash_drive in self.connected_drives
                                  if flash_drive in curr_connected_flash_drives]
 
-    def deal(self, flash_drive, dir_name):
+    def deal(self, dir_path):
+        # delete file, print file
         # show gui
-        # self.window.deiconify()
-        # time.sleep(1)
+        self.display_menu()
+        print(f"Dealing with bad file at {dir_path}")
+        with open(dir_path, "r") as file:
+            try:
+                print(file.read())
+            except Exception:
+                print("cant read file")
+
+
+    def display_menu(self):
+        pass
         # self.window.withdraw()
-        print(f"Dealing with bad file at {flash_drive.name}/{dir_name}")
+        # self.window.title("AntiVirus")
+        # self.window.minsize(height=300, width=500)
+        # label = Label(self.window, text="AntiVirus")
+        # button1 = Button(self.window, text="Scan", command=lambda: print(1))
+        # button2 = Button(self.window, text="Update", command=lambda: print(2))
+        # button3 = Button(self.window, text="Quarantine", command=lambda: print(3))
+        # button4 = Button(self.window, text="Settings", command=lambda: print(4))
+        #
+        # label.grid(row=0, column=0, columnspan=2)
+        # button1.grid(row=1, column=0, padx=10, pady=10)
+        # button2.grid(row=1, column=1, padx=10, pady=10)
+        # button3.grid(row=2, column=0, padx=10, pady=10)
+        # button4.grid(row=2, column=1, padx=10, pady=10)
